f1cb9b7491cd18be7dc1223ead540275347ab575
CannelleJP <isaibmezghiche@edenschool.fr>
2022-09-21 14:43:22 +0200

    TYPE DE VARIABLE ET OPERATIONS SUR LES VARIABLES

--- js/app.js
+++ js/app.js
@@ -1,4 +1,6 @@
+toggleFullscreen();
 console.log('hello')
+
 
 let age;
 age = 14;   
@@ -6,5 +8,41 @@ age = 14;
 console.log (typeof age) 
 const prenom = "ilyan";
 // prenom est une varaible "string"
+let adition = 4+12;
+console.log(adition);
+
+let soustraction = 2-50;
+console.log(soustraction);
+
+let multiplication = 3*8;
+console.log(multiplication);
+
+let division = 7/2;
+console.log(division);
+
+let modulo = 5%2;
+console.log(modulo);
+
+let test = 3>5;
+console.log(test);  
+
+console.log(typeof test );
+
+// test est une variable de type "booléen"
+// === <=  >= > <  == !== !=
+
+const indentity = "Bonjour" + " " + prenom + " " + "vous avez" + " " + age + " " + "ans"
+
+// cette opération est une concaténation
+
+
+
+let majuscule = indentity.toUpperCase();
+
+console.log("majuscule:",majuscule);
+console.log("indentity:",indentity);
+
+// la variable identity n'a pas été modifié mais la fonction toUpperCase a été appliqué à majuscule
+
 
 
