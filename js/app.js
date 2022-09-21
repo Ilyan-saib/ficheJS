f70873484ac1d5b58bca76d24f69deb469e267e2
CannelleJP <isaibmezghiche@edenschool.fr>
2022-09-21 15:18:06 +0200

    conditions

--- js/app.js
+++ js/app.js
@@ -1,32 +1,32 @@
-toggleFullscreen();
+
 console.log('hello')
 
 
 let age;
-age = 14;   
+age = 14;
 // age est une variable "number"
-console.log (typeof age) 
+console.log(typeof age)
 const prenom = "ilyan";
 // prenom est une varaible "string"
-let adition = 4+12;
+let adition = 4 + 12;
 console.log(adition);
 
-let soustraction = 2-50;
+let soustraction = 2 - 50;
 console.log(soustraction);
 
-let multiplication = 3*8;
+let multiplication = 3 * 8;
 console.log(multiplication);
 
-let division = 7/2;
+let division = 7 / 2;
 console.log(division);
 
-let modulo = 5%2;
+let modulo = 5 % 2;
 console.log(modulo);
 
-let test = 3>5;
-console.log(test);  
+let test = 3 > 5;
+console.log(test);
 
-console.log(typeof test );
+console.log(typeof test);
 
 // test est une variable de type "booléen"
 // === <=  >= > <  == !== !=
@@ -39,10 +39,28 @@ const indentity = "Bonjour" + " " + prenom + " " + "vous avez" + " " + age + " "
 
 let majuscule = indentity.toUpperCase();
 
-console.log("majuscule:",majuscule);
-console.log("indentity:",indentity);
+console.log("majuscule:", majuscule);
+console.log("indentity:", indentity);
 
 // la variable identity n'a pas été modifié mais la fonction toUpperCase a été appliqué à majuscule
 
 
+age = prompt("Veuillez saisir votre age");
+if (age <= 0) {
+    console.log("vous ete un poisson pané");
+}
+if (age > 0 && age < 120) {
+    console.log(age)
+    console.log("votre age est validé");
+}
+else {
+    console.log('je ne suis pas d accord');
+}
 
+if (age >= 21) {
+    console.log('vous etes majeur');
+} else if (age < 18) {
+    console.log("vous etes mineur");
+} else {
+    console.log("vous etes majeurs mais pas au usa");
+}
